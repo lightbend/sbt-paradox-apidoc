84198ccea4b3a7d7da44b6fd21a03a93098f3a27
Martynas Mickevičius <self@2m.lt>
2019-06-18 08:38:24 +0300

    Reformat code

--- src/main/scala/com/lightbend/paradox/apidoc/ApidocPlugin.scala
+++ src/main/scala/com/lightbend/paradox/apidoc/ApidocPlugin.scala
@@ -40,26 +40,27 @@ object ApidocPlugin extends AutoPlugin {
   def apidocParadoxGlobalSettings: Seq[Setting[_]] = Seq(
     apidocRootPackage := "scala",
     paradoxDirectives ++= Def.taskDyn {
-      val classpath   = (fullClasspath in Compile).value.files.map(_.toURI.toURL).toArray
-      val classLoader = new java.net.URLClassLoader(classpath, this.getClass.getClassLoader)
-      val scanner = new ClassGraph()
-        .whitelistPackages(apidocRootPackage.value)
-        .addClassLoader(classLoader)
-        .scan()
-      val allClasses = scanner.getAllClasses.getNames.asScala.toVector
-      Def.task {
-        Seq(
-          { _: Writer.Context ⇒
-            new ApidocDirective(allClasses)
+          val classpath   = (fullClasspath in Compile).value.files.map(_.toURI.toURL).toArray
+          val classLoader = new java.net.URLClassLoader(classpath, this.getClass.getClassLoader)
+          val scanner = new ClassGraph()
+            .whitelistPackages(apidocRootPackage.value)
+            .addClassLoader(classLoader)
+            .scan()
+          val allClasses = scanner.getAllClasses.getNames.asScala.toVector
+          Def.task {
+            Seq(
+              { _: Writer.Context ⇒
+                new ApidocDirective(allClasses)
+              }
+            )
           }
-        )
-      }
-    }.value
+        }.value
   )
 
   def apidocSettings(config: Configuration): Seq[Setting[_]] =
     apidocParadoxGlobalSettings ++ inConfig(config)(
-      Seq(
-        // scoped settings here
-      ))
+          Seq(
+            // scoped settings here
+          )
+        )
 }
