86e0d840383262b1207638b12009ee13c530211f
Scala Steward <me@scala-steward.org>
2019-10-18 05:03:04 +0200

    Update classgraph to 4.8.50

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Library {
-  val classgraph = "io.github.classgraph"  % "classgraph"  % "4.8.47"
+  val classgraph = "io.github.classgraph"  % "classgraph"  % "4.8.50"
   val jtidy      = "net.sf.jtidy"          % "jtidy"       % "r938"
   val sbtParadox = "com.lightbend.paradox" % "sbt-paradox" % "0.6.6"
   val scalatest  = "org.scalatest"         %% "scalatest"  % "3.0.8"
