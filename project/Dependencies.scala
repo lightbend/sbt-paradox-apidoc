456914984c2b624d826b12e9ed82f266408467a1
Enno <458526+ennru@users.noreply.github.com>
2019-11-21 15:39:19 +0100

    Paradox 0.6.7 (#85)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Library {
   val classgraph = "io.github.classgraph"  % "classgraph"  % "4.8.54"
   val jtidy      = "net.sf.jtidy"          % "jtidy"       % "r938"
-  val sbtParadox = "com.lightbend.paradox" % "sbt-paradox" % "0.6.6"
+  val sbtParadox = "com.lightbend.paradox" % "sbt-paradox" % "0.6.7"
   val scalatest  = "org.scalatest"         %% "scalatest"  % "3.0.8"
 }
