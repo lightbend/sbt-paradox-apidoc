561e212de8153dd345ca7ab73e53ab9cc02d1d0c
Scala Steward <me@scala-steward.org>
2020-04-24 20:31:56 +0200

    Update sbt-paradox to 0.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Library {
   val classgraph = "io.github.classgraph"  % "classgraph"  % "4.8.75"
   val jtidy      = "net.sf.jtidy"          % "jtidy"       % "r938"
-  val sbtParadox = "com.lightbend.paradox" % "sbt-paradox" % "0.6.9"
+  val sbtParadox = "com.lightbend.paradox" % "sbt-paradox" % "0.8.0"
   val scalatest  = "org.scalatest"         %% "scalatest"  % "3.1.1"
 }
