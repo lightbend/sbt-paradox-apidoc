c658fa7ef6968e2efe0bc2ae4ff8b962ec86a457
Scala Steward <me@scala-steward.org>
2020-06-26 08:42:02 +0200

    classgraph 4.8.87 (was 4.8.78)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Library {
-  val classgraph     = "io.github.classgraph"  % "classgraph"  % "4.8.78"
+  val classgraph     = "io.github.classgraph"  % "classgraph"  % "4.8.87"
   val sbtParadox     = "com.lightbend.paradox" % "sbt-paradox" % "0.8.0"
   val scalatest      = "org.scalatest"         %% "scalatest"  % "3.1.2"
   val paradoxTestkit = "com.lightbend.paradox" %% "testkit"    % "0.8.0"
