6ff8ac3d9d17ecdcd967bc4a760797494f0da9b6
Enno Runne <458526+ennru@users.noreply.github.com>
2021-12-15 13:24:40 +0100

    Current versions; drop CI publishing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Library {
-  val classgraph     = "io.github.classgraph"   % "classgraph"  % "4.8.90"
-  val sbtParadox     = "com.lightbend.paradox"  % "sbt-paradox" % "0.8.0"
-  val scalatest      = "org.scalatest"         %% "scalatest"   % "3.2.2"
-  val paradoxTestkit = "com.lightbend.paradox" %% "testkit"     % "0.8.0"
+  val classgraph     = "io.github.classgraph"   % "classgraph"  % "4.8.133"
+  val sbtParadox     = "com.lightbend.paradox"  % "sbt-paradox" % "0.9.2"
+  val scalatest      = "org.scalatest"         %% "scalatest"   % "3.2.10"
+  val paradoxTestkit = "com.lightbend.paradox" %% "testkit"     % "0.9.2"
 }
