f9d60b4e307a3459ce559193f0cc3c3d4175ecb3
Scala Steward <scala_steward@virtuslab.com>
2022-08-12 16:12:31 +0000

    Revert commit(s) 3c97e7c

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Library {
   val classgraph     = "io.github.classgraph"   % "classgraph"  % "4.8.141"
-  val sbtParadox     = "com.lightbend.paradox"  % "sbt-paradox" % "0.10.2"
+  val sbtParadox     = "com.lightbend.paradox"  % "sbt-paradox" % "0.9.2"
   val scalatest      = "org.scalatest"         %% "scalatest"   % "3.2.10"
-  val paradoxTestkit = "com.lightbend.paradox" %% "testkit"     % "0.10.2"
+  val paradoxTestkit = "com.lightbend.paradox" %% "testkit"     % "0.9.2"
 }
