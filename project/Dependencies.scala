c570ff09e0e6f9366379161169b32e173e345116
Scala Steward <scala_steward@virtuslab.com>
2022-10-19 21:11:00 +0000

    sbt-paradox, testkit 0.10.2 (was 0.9.2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Library {
   val classgraph     = "io.github.classgraph"   % "classgraph"  % "4.8.149"
-  val sbtParadox     = "com.lightbend.paradox"  % "sbt-paradox" % "0.9.2"
+  val sbtParadox     = "com.lightbend.paradox"  % "sbt-paradox" % "0.10.2"
   val scalatest      = "org.scalatest"         %% "scalatest"   % "3.2.14"
-  val paradoxTestkit = "com.lightbend.paradox" %% "testkit"     % "0.9.2"
+  val paradoxTestkit = "com.lightbend.paradox" %% "testkit"     % "0.10.2"
 }
