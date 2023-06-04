e01496391d9c416e049a11282381806df085ee23
Scala Steward <scala_steward@virtuslab.com>
2023-06-04 20:44:07 +0000

    scalatest 3.2.16 (was 3.2.14)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Library {
   val classgraph     = "io.github.classgraph"   % "classgraph"  % "4.8.153"
   val sbtParadox     = "com.lightbend.paradox"  % "sbt-paradox" % "0.10.3"
-  val scalatest      = "org.scalatest"         %% "scalatest"   % "3.2.14"
+  val scalatest      = "org.scalatest"         %% "scalatest"   % "3.2.16"
   val paradoxTestkit = "com.lightbend.paradox" %% "testkit"     % "0.10.3"
 }
